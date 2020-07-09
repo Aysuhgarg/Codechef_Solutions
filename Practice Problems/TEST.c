df9907a351272731fabc87a9028dd9fd1e832b29
Aysuhgarg <Coolayushgarg222@gmail.com>
2020-07-10 00:25:27 +0530

    Source code

---
+++ Practice Problems/TEST.c
@@ -0,0 +1,13 @@
+//Problem Statement --------------->https://www.codechef.com/problems/TEST
+
+-----------------Solution-----------------
+#include<stdio.h>
+int main(){
+	int c;
+	while(1){
+		scanf("%d",&c);
+		if(c==42)
+			break;
+		printf("%d\n",c);
+	}
+return 0;}
