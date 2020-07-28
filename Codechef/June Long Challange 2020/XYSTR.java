de63cdba727836ca3af193ad6a88ca41c6238946
Aysuhgarg <Coolayushgarg222@gmail.com>
2020-07-29 00:04:04 +0530

    Source code

--- Codechef/June Long Challange 2020/XYSTR.java
+++ Codechef/June Long Challange 2020/XYSTR.java
@@ -1,14 +1,9 @@
 Problem Statement------------->https://www.codechef.com/JUNE20B/problems/XYSTR
 
 -------------------------->Solution<------------------------
-
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
