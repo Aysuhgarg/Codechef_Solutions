2c176ef814f4d9a3929e591c2b1f83903d08ecc6
Aysuhgarg <Coolayushgarg222@gmail.com>
2020-07-30 21:27:50 +0530

    Source code

--- Codechef/Practice Problems/JOHNY.java
+++ Codechef/Practice Problems/JOHNY.java
@@ -1,15 +1,9 @@
-Problem Statement---------->https://www.codechef.com/problems/JOHNY
+//Problem Statement---------->https://www.codechef.com/problems/JOHNY
 
------------------------Solution----------------
-
-/* package codechef; // don't place package name! */
-/* package codechef; // don't place package name! */
-
+//-----------------------Solution----------------
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
