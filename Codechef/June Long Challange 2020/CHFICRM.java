b68a4f3d1609ab4ffde8e305378eb6ec9a1839d7
Aysuhgarg <Coolayushgarg222@gmail.com>
2020-07-30 21:25:42 +0530

    Source code

--- Codechef/June Long Challange 2020/CHFICRM.java
+++ Codechef/June Long Challange 2020/CHFICRM.java
@@ -1,17 +1,12 @@
 //problem statement-------->https://www.codechef.com/JUNE20B/problems/CHFICRM
 
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner sc=new Scanner (System.in);
 		int t;
 		t=sc.nextInt();
