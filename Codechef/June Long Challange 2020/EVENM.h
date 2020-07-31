bd09c6314cc40705dccf559822fb79f1e965a1ef
Aysuhgarg <Coolayushgarg222@gmail.com>
2020-07-31 23:22:05 +0530

    Source code

--- Codechef/June Long Challange 2020/EVENM.h
+++ Codechef/June Long Challange 2020/EVENM.h
@@ -6,17 +6,13 @@ Problem Statement------------>https://www.codechef.com/JUNE20B/problems/EVENM
 using namespace std;
 
 int main() {
-	// your code goes here
-	
 	int t;
-		//t=sc.nextInt();
 		scanf("%d",&t);
 		while(t-->0)
 		{
 		    int n,k=1;
-		  //  n=sc.nextInt();
 		    	scanf("%d",&n);
-		    int a[n+1][n+1];//=new int[n+1][n+1];
+		    int a[n+1][n+1];
 		    for(int i=1;i<=n;i++)
 		    {
 		        if(i%2==0)
