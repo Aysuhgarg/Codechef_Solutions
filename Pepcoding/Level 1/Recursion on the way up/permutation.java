41bde5bd2630a5d7b5304c1e75539dadf02e22f3
Aysuhgarg <Coolayushgarg222@gmail.com>
2021-05-30 14:31:41 +0530

    permutation

---
+++ Pepcoding/Level 1/Recursion on the way up/permutation.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.util.*;
+
+public class permutation {
+
+    public static void main(String[] args) throws Exception {
+    Scanner sc=new Scanner (System.in);
+    String str=sc.next();
+    printPermutations(str,"");
+    }
+
+    public static void printPermutations(String str, String asf) {
+        
+        if(str.length()==0)
+        {
+            System.out.println(asf);
+            return;
+        }
+        for(int i=0;i<str.length();i++)
+        {
+        String ros=str.substring(0,i)+str.substring(i+1);
+        printPermutations(ros,asf+str.charAt(i));
+        }
+    }
+
+}
